08d874870b904e2397f17864dfcae422ccc8ee9d
Miyamotto <105276248+Miyamotto@users.noreply.github.com>
2022-09-20 19:49:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction() {
+  document.getElementById("featuresMenu").classList.toggle("show");
+  document.getElementById("shown").classList.toggle("show");
+  document.getElementById("hidden").classList.toggle("show");
+}
+function myFunctionOne() {
+  document.getElementById("companyMenu").classList.toggle("show");
+  document.getElementById("shownOne").classList.toggle("show");
+  document.getElementById("hiddenOne").classList.toggle("show");
+}
+function myFunctionTwo() {
+  document.getElementById("hamburgerShow").classList.toggle("show");
+  document.getElementById("hamburgerclose").classList.toggle("show");
+}
