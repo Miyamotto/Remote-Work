82825ebf9d2babc6b87a0dbf5c89d86cbbcdadd0
Miyamotto <105276248+Miyamotto@users.noreply.github.com>
2022-09-22 22:19:08 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function myFunction() {
-  document.getElementById("featuresMenu").classList.toggle("show");
-  document.getElementById("shown").classList.toggle("show");
-  document.getElementById("hidden").classList.toggle("show");
+document.getElementById("featuresOne").addEventListener("click", openMenu);
+function openMenu() {
+  document.getElementById("featuresHidden").classList.toggle("show");
+  document.getElementById("downArrow").classList.toggle("show");
+  document.getElementById("upArrow").classList.toggle("show");
 }
-function myFunctionOne() {
-  document.getElementById("companyMenu").classList.toggle("show");
-  document.getElementById("shownOne").classList.toggle("show");
-  document.getElementById("hiddenOne").classList.toggle("show");
+document.getElementById("companyOne").addEventListener("click", openMenuOne);
+function openMenuOne() {
+  document.getElementById("companyHidden").classList.toggle("show");
+  document.getElementById("downArrow1").classList.toggle("show");
+  document.getElementById("upArrow1").classList.toggle("show");
 }
-function myFunctionTwo() {
-  document.getElementById("hamburgerShow").classList.toggle("show");
-  document.getElementById("hamburgerclose").classList.toggle("show");
+
+document.getElementById("open").addEventListener("click", openMenuTwo);
+function openMenuTwo() {
+  document.getElementById("open").classList.toggle("show");
+  document.getElementById("close").classList.toggle("show");
+  document.getElementById("showNav").classList.toggle("show");
+  document.getElementById("navItems").classList.toggle("show");
+  document.getElementById("navList").classList.toggle("show");
+}
+document.getElementById("close").addEventListener("click", openMenuthree);
+function openMenuthree() {
+  document.getElementById("open").classList.toggle("show");
+  document.getElementById("close").classList.toggle("show");
+  document.getElementById("showNav").classList.toggle("show");
+  document.getElementById("navItems").classList.toggle("show");
+  document.getElementById("navList").classList.toggle("show");
 }
